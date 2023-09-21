0530c8d7cdb18425e9400b8883262385ad8c6fc9
ShaneChen0219 <hsiangyuan0219@gmail.com>
2023-09-21 15:45:38 -0600

    die koopa

--- src/SuperMario.cpp
+++ src/SuperMario.cpp
@@ -26,57 +26,53 @@ int main()
     Renderer renderer(window);
     Map map;
     Mario mario;
-//    Generate koopas
+    //    Generate koopas
     int pos = rand()%800 +40;
-    int speed = rand()%5 +1;
-    
+    float speed = rand()%5 +1;
+    int numOfKoopaAndGoomba =10;
     std::vector<Koopa> koopas;
-    for(int i= 0; i<2;i++){
-            pos = rand()%800 +40;
-            speed = rand()%5 +1;
-            Koopa koopa_obj(pos,speed);
-            koopas.push_back(koopa_obj);
-         
-        }
-
-    
-//    Koopa any;
+    for(int i= 0; i<numOfKoopaAndGoomba;i++){
+        srand(i+speed);
+        pos = rand()%800 +40;
+        speed = (rand()% 5000 +1000)/1000;
+        Koopa koopa_obj(pos,speed);
+        koopas.push_back(koopa_obj);
+    }
+    //only for making the image right
+    Koopa any;
     std::vector<Goomba> goombas;
-    for(int i= 0; i<2;i++){
-            pos = rand()%800 +40;
-            speed = rand()%5 +1;
-            Goomba goombas_obj(pos,speed);
-            goombas.push_back(goombas_obj);
-         
-        }
+    for(int i= 0; i<numOfKoopaAndGoomba;i++){
+        srand(i+speed);
+        pos = rand()%800 +40;
+        speed = (rand()% 5000 +1000)/1000;
+        Goomba goombas_obj(pos,speed);
+        goombas.push_back(goombas_obj);
+        
+    }
+    //only for making the image right
     Goomba any2;
     
-//    pos = rand()%800 +40;
-//    speed = rand()%5 +1;
-//    Koopa koopa(pos,speed);
-//    pos = rand()%800 +40;
-//    speed = rand()%5 +1;
-// Koopa koopaiijkjkhkhjh(pos,speed);
-//    pos = rand()%800 +40;
-//    speed = rand()%5 +1;
-//    Koopa koopa2(pos,speed);
+    //    pos = rand()%800 +40;
+    //    speed = rand()%5 +1;
+    //    Koopa koopa(pos,speed);
+    //    pos = rand()%800 +40;
+    //    speed = rand()%5 +1;
+    // Koopa koopaiijkjkhkhjh(pos,speed);
+    //    pos = rand()%800 +40;
+    //    speed = rand()%5 +1;
+    //    Koopa koopa2(pos,speed);
     
-//    std::vector<Koopa> koopas = {koopa[0],koopa[1]};
-//    pos = rand()%800 +40;
-//    Goomba goomba(pos);
-//    pos = rand()%800 +40;
-//    Goomba goomba1(pos);
-//    pos = rand()%800 +40;
-//    Goomba goomba2(pos);
-//    std::vector<Goomba> goombas = {goomba,goomba1,goomba2};
+    //    std::vector<Koopa> koopas = {koopa[0],koopa[1]};
+    //    pos = rand()%800 +40;
+    //    Goomba goomba(pos);
+    //    pos = rand()%800 +40;
+    //    Goomba goomba1(pos);
+    //    pos = rand()%800 +40;
+    //    Goomba goomba2(pos);
+    //    std::vector<Goomba> goombas = {goomba,goomba1,goomba2};
     GameOver gameover;
     Score score;
     int timesRumming =0;
-    //    JumpStage jumpstage;
-    //    JumpStage jumpstage1(sf::Vector2f{200,150});
-    //    JumpStage jumpstage2(sf::Vector2f{250,200});
-    //    std::vector<JumpStage> allJumpStage={jumpstage,jumpstage1,jumpstage2};
-    
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -91,55 +87,42 @@ int main()
         deltaTime = deltaTimeClock.restart();
         
         window.clear(sf::Color::White);
-        //        if(mario._isalive ==true){
-        
-        map.drawMap(renderer);
-        mario.UpdateMario(map);
-        mario.drawMario(window);
-//        koopa1.drawKoopa(window);
-//        koopa1.Move(map);
-
-        for (int i = 0; i<2 ;i++) {
-            koopas[i].drawKoopa(window);
-            koopas[i].Move(map);
-            goombas[i].drawGoomba(window);
-            goombas[i].Move(map);
-//            std::cout<<koopas[i]._koopaPos.x<<"\n";
-//            std::cout<<koopas[i]._koopaPos.y<<"\n";
-//            std::cout<<koopas[i]._isJumped<<"\n";
+        if(mario._isalive ==true){
             
-        }
-      
-
-        
-
-        //            koopa.JumpedOnTop(mario);
-        
-        
-//        goomba.drawGoomba(window);
-        //            goomba.JumpedOnTop(mario);
-//        goomba.Move(map);
-        
-//        score.update(koopa,goomba,mario);
-        score.drawScores(window);
-        
-        if (timesRumming>=500) {
+            map.drawMap(renderer);
+            mario.UpdateMario(map);
+            mario.drawMario(window);
             
-//            koopa.reset();
-//            goomba.reset();
-            timesRumming=0;
+            for (int i = 0; i<numOfKoopaAndGoomba ;i++) {
+                koopas[i].drawKoopa(window);
+                koopas[i].Move(map);
+                goombas[i].drawGoomba(window);
+                goombas[i].Move(map);
+            }
+            
+            //        koopa.JumpedOnTop(mario);
+            //        goomba.JumpedOnTop(mario);
+            
+            score.drawScores(window);
+            
+            if (timesRumming>=500) {
+                for (int i = 0; i<numOfKoopaAndGoomba ;i++) {
+                    koopas[i].reset();
+                    goombas[i].reset();
+                }
+                timesRumming=0;
+            }
+            for (int i = 0; i<numOfKoopaAndGoomba ;i++) {
+                score.update(koopas[i],goombas[i],mario);
+                mario.GameOver(goombas[i],koopas[i]);
+            }
+            score.drawScores(window);
+            timesRumming++;
+            
+        }else{
+            gameover.drawGameOver(window);
+            score.drawScores(window);
         }
-        
-//        mario.GameOver(goomba, koopa);
-//        mario.GameOver(goomba1, koopa1);
-        //            mario.GameOver(goomba2, koopa2);
-        score.drawScores(window);
-        timesRumming++;
-        
-        //        }else{
-        //            gameover.drawGameOver(window);
-        //            score.drawScores(window);
-        //        }
         // end the current frame
         window.display();
     }
